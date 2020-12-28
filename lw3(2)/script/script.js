23a5b44d579631620b494d8d5bc6dc7a2998ab3c
sasha_ivanova <sashaivanova1801@gmail.com>
2020-12-29 00:32:26 +0300

    uploading lw3 on a github

---
+++ lw3(2)/script/script.js
@@ -0,0 +1,126 @@
+ 
+  
+   
+    
+     window.onload = function() {
+
+  var listingElements = ['apple', 'orange'];
+  var storeElements = [];
+
+  var isInAscending = true;
+
+  function addToStoreElements(element) {
+    var elementPosition = listingElements.indexOf(element);
+    if (elementPosition > -1) {
+      storeElements.push(element);
+      listingElements.splice(elementPosition, 1);
+    }
+  }
+
+  function addToListingElements(element) {
+    var elementPosition = storeElements.indexOf(element);
+    if (elementPosition > -1) {
+      listingElements.push(element);
+      storeElements.splice(elementPosition, 1);
+    }
+  }
+
+  function deleteFromElements(list, element) {
+    var elementPosition = list.indexOf(element);
+    if (elementPosition > -1) {
+      list.splice(elementPosition, 1);
+    }
+  }
+
+  function newAddToListingElements(element) {
+    var elementPosition = storeElements.indexOf(element);
+    if (elementPosition > -1) {
+      return -1;
+    }
+    elementPosition = listingElements.indexOf(element);
+    if (elementPosition > -1) {
+      return -1;
+    }
+    listingElements.push(element);
+    return 1;
+  }
+
+  function renameElement(element, newValue) {
+    var elementPosition = storeElements.indexOf(element);
+    if (elementPosition > -1) {
+      storeElements[elementPosition] = newValue;
+    }
+    elementPosition = listingElements.indexOf(element);
+    if (elementPosition > -1) {
+      listingElements[elementPosition] = newValue;
+    }
+  }
+
+  function updateUI() {
+    var storeSelect = document.querySelector('.store-select');
+    var listingSelect = document.querySelector('.listing-select');
+    storeSelect.innerHTML = '';
+    listingSelect.innerHTML = '';
+
+    for (var i = 0; i < listingElements.length; i++) {
+      var newOption = document.createElement('option');
+      newOption.innerText = listingElements[i];
+      listingSelect.append(newOption);
+    }
+
+    for (var i = 0; i < storeElements.length; i++) {
+      var newOption = document.createElement('option');
+      newOption.innerText = storeElements[i];
+      storeSelect.append(newOption);
+    }
+  }
+
+  var addStoreButton = document.querySelector('#add-store-button');
+
+  addStoreButton.onclick = function() {
+    var selectedOption = document.querySelector('.listing-select option:checked');
+    addToStoreElements(selectedOption.innerText);
+    updateUI();
+  }
+
+  var addListingButton = document.querySelector('#add-listing');
+
+  addListingButton.onclick = function() {
+    var selectedOption = document.querySelector('.store-select option:checked');
+    addToListingElements(selectedOption.innerText);
+    updateUI();
+  }
+
+  var deleteButton = document.querySelector('#delete-element');
+
+  deleteButton.onclick = function() {
+    var selectedOption = document.querySelector('.store-select option:checked');
+    if (selectedOption !== null) {
+      deleteFromElements(storeElements, selectedOption.innerText);
+    } else {
+      selectedOption = document.querySelector('.listing-select option:checked');
+      deleteFromElements(listingElements, selectedOption.innerText);
+    }
+    updateUI();
+  }
+
+  var newElementButton = document.querySelector('#new-element');
+
+  newElementButton.onclick = function() {
+    var newElement = prompt("Введите название нового элемента");
+    var result = newAddToListingElements(newElement);
+    updateUI();
+  }
+
+  var renameButton = document.querySelector('#rename');
+
+  renameButton.onclick = function() {
+    var selectedOption = document.querySelector('.store-select option:checked');
+    if (selectedOption === null) {
+      selectedOption = document.querySelector('.listing-select option:checked');
+    }
+    var newValue = prompt("Новое название элемента");
+    renameElement(selectedOption.innerText, newValue);
+    updateUI();
+  }
+};
